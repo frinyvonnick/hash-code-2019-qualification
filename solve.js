c8a26bccf87cba05d1888950e89bff342d2130aa
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 19:27:43 +0100

    tayo!

--- solve.js
+++ solve.js
@@ -9,9 +9,15 @@ const gridUtils = require("./grid-utils");
 //   { tags: ["garden", "cat"], vertical: false }
 // ];
 
+function mapId(photo) {
+  return photo.id;
+}
+
 function solve(problem, file) {
-  const horizontals = problem.filter(photo => !photo.vertical);
-  const verticals = _.chunk(problem.filter(photo => photo.vertical), 2);
+  const horizontals = problem.filter(photo => !photo.vertical).map(mapId);
+  const verticals = _.chunk(problem.filter(photo => photo.vertical), 2).map(
+    mapId
+  );
 
   return [
     horizontals.length + verticals.length,
