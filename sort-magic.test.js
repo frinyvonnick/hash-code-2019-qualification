98284576688be74e7ff21a9992e9ec7dba743e67
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 21:43:55 +0100

    tayo!

--- sort-magic.test.js
+++ sort-magic.test.js
@@ -1,12 +1,10 @@
 /* eslint-env mocha */
 
-const assert = require('assert')
-const sortMagic = require('./sortMagic')
+const assert = require("assert");
+const sortMagic = require("./sortMagic");
 
-describe('sortMagic', function () {
-  it('sortMagics', function () {
-    assert.deepEqual(
-      sortMagic(),
-      undefined)
-  })
-})
+describe("sortMagic", function() {
+  it("sortMagics", function() {
+    assert.deepEqual(sortMagic(), undefined);
+  });
+});
