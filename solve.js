5bbd8d783254c5f6adadc039c42701f8c281a9cb
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 20:14:52 +0100

    commit

--- solve.js
+++ solve.js
@@ -2,13 +2,6 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
-// const photos = [
-//   { tags: ["cat", "beach", "sun"], vertical: true },
-//   { tags: ["selfie", "smile"], vertical: true },
-//   { tags: ["garden", "selfie"], vertical: true },
-//   { tags: ["garden", "cat"], vertical: false }
-// ];
-
 function mapId(photo) {
   return photo.id;
 }
@@ -25,7 +18,5 @@ function solve(problem, file) {
     ...[...horizontals, ...verticals.map(l => l.join(" "))]
   ];
 }
-
-// console.log(solve(photos));
 
 module.exports = solve;
