e00876cc193c062fe47b44b5c250f8a6ba46ba58
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 19:05:56 +0100

    solve v1

--- solve.js
+++ solve.js
@@ -2,9 +2,18 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
+// const photos = [
+//   { tags: ["cat", "beach", "sun"], vertical: true },
+//   { tags: ["selfie", "smile"], vertical: true },
+//   { tags: ["garden", "selfie"], vertical: true },
+//   { tags: ["garden", "cat"], vertical: false }
+// ];
+
 function solve(problem, file) {
   // destructure this!
-  return [];
+  return [1, problem.findIndex(photo => !photo.vertical)];
 }
+
+// console.log(solve(photos));
 
 module.exports = solve;
