1342a136494749a10cde27f4dd875f5714bfeffe
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 22:21:51 +0100

    tayo!

--- sort-magic.js
+++ sort-magic.js
@@ -32,7 +32,7 @@ module.exports = function sortMagic(slideshow) {
     total: slideshow.length
   });
 
-  const filteredSlideShow = slideshow.filter(slide => slide.tags.length > 28);
+  const filteredSlideShow = slideshow.filter(slide => slide.tags.length > 26);
 
   const result = [];
   const length = filteredSlideShow.length;
