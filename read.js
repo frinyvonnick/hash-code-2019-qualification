f42cb00bc9d050be5d02deca04974ee92f0674f9
Sébastien Romain <sromain@tlnd-sromain.local>
2019-02-28 19:37:50 +0100

    tayo!

--- read.js
+++ read.js
@@ -6,30 +6,30 @@ const jolicitron = require("jolicitron");
 
 module.exports = function read(filePath) {
   const cachedFile = `${filePath.split(".")[0]}.in.json`;
-  try {
-    fs.accessSync(cachedFile);
-    debug(`using cached ${cachedFile}`);
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      debug(
-        `not using cached input file because it does not exist at: ${cachedFile}`
-      );
-    } else {
-      debug(`not using cached input file because:`, err);
-    }
-    const textFromInputFile = fs.readFileSync(filePath, "utf8");
-    debug(`read ${textFromInputFile.length} chars from ${filePath}`);
-    const result = module.exports.parse(textFromInputFile);
-    fs.writeFileSync(cachedFile, JSON.stringify(result));
-    debug(`written cached input file to ${cachedFile}`);
-    return result;
-  }
-  return require(`./${cachedFile}`);
+  // try {
+  //   fs.accessSync(cachedFile);
+  //   debug(`using cached ${cachedFile}`);
+  // } catch (err) {
+  //   if (err.code === "ENOENT") {
+  //     debug(
+  //       `not using cached input file because it does not exist at: ${cachedFile}`
+  //     );
+  //   } else {
+  //     debug(`not using cached input file because:`, err);
+  //   }
+  const textFromInputFile = fs.readFileSync(filePath, "utf8");
+  debug(`read ${textFromInputFile.length} chars from ${filePath}`);
+  const result = module.exports.parse(textFromInputFile);
+  fs.writeFileSync(cachedFile, JSON.stringify(result));
+  debug(`written cached input file to ${cachedFile}`);
+  return result;
+  // }
+  // return require(`./${cachedFile}`);
 };
 
 const parse = inputText => {
+  console.log(inputText);
   const [number, ...photos] = inputText.split("\n");
-
   return photos.map((photo, id) => {
     const [mode, n, ...tags] = photo.split(" ");
     return {
